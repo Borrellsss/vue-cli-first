07c3dbf59fcdc94f9bd92d9dd5183b2c662e08be
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-07-05 20:22:16 +0200

    exercise done

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{test}}</h1>
+</template>
+
+<script>
+export default {
+    name: "TitleComponent",
+    props: {
+        test: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        font-size: 4rem;
+    }
+</style>
