86ff8fe8fba967b9deb4c2fc43eb589e819f16ff
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-07-05 21:02:18 +0200

    bonus done

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,32 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <TitleComponent test="La mia prima app con Vue CLI!"/>
+    <HeroComponent/>
   </div>
 </template>
 
 <script>
-import TitleComponent from "./components/TitleComponent.vue"
+import TitleComponent from "./components/TitleComponent.vue";
+import HeroComponent from "./components/HeroComponent.vue";
 
 export default {
   name: 'App',
   components: {
     TitleComponent,
+    HeroComponent,
   }
 }
 </script>
 
 <style lang="scss">
+/* *RESET* */
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;   
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="jumbotron">
+        <img src="http://samcharter.com/wp-content/uploads/2019/06/NASA.jpg" alt="Evangelion Wallpaper">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroComponent",
+    props: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .jumbotron {
+        margin: 2rem 0;
+        width: 100vw;
+    }
+    img {
+        width: 100%;
+        display: block;
+    }
+</style>
